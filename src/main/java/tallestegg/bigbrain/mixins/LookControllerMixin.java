a7798f456929246b6649ae3d340931a32a745f65
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2021-02-05 12:30:41 -0800

    removed @Overwrites in lookcontroller and movecontroller mixins
    a forge event is still preferred

--- src/main/java/tallestegg/bigbrain/mixins/LookControllerMixin.java
+++ src/main/java/tallestegg/bigbrain/mixins/LookControllerMixin.java
@@ -2,13 +2,14 @@ package tallestegg.bigbrain.mixins;
 
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.controller.LookController;
-import net.minecraft.util.math.MathHelper;
 import tallestegg.bigbrain.BigBrainEnchantments;
 import tallestegg.bigbrain.entity.IBucklerUser;
 
@@ -18,82 +19,17 @@ public class LookControllerMixin {
     @Final
     protected MobEntity mob;
 
-    @Shadow
-    protected float deltaLookYaw;
-
-    @Shadow
-    protected float deltaLookPitch;
-
-    @Shadow
-    protected boolean isLooking;
-
-    @Shadow
-    protected double posX;
-
-    @Shadow
-    protected double posY;
-
-    @Shadow
-    protected double posZ;
-
-    // TODO PR a event to forge that allows us to cancel look control ticking, and also pray that they don't ignore it.
-    @Overwrite
-    public void tick() {
-        if (EnchantmentHelper.getEnchantmentLevel(BigBrainEnchantments.TURNING.get(), mob.getHeldItemOffhand()) > 0 && ((IBucklerUser) mob).isBucklerDashing() || !((IBucklerUser) mob).isBucklerDashing()) {
-            if (this.shouldResetPitch()) {
-                this.mob.rotationPitch = 0.0F;
-            }
-
-            if (this.isLooking) {
-                this.isLooking = false;
-                this.mob.rotationYawHead = this.clampedRotate(this.mob.rotationYawHead, this.getTargetYaw(), this.deltaLookYaw);
-                this.mob.rotationPitch = this.clampedRotate(this.mob.rotationPitch, this.getTargetPitch(), this.deltaLookPitch);
-            } else {
-                this.mob.rotationYawHead = this.clampedRotate(this.mob.rotationYawHead, this.mob.renderYawOffset, 10.0F);
-            }
-
-            if (!this.mob.getNavigator().noPath()) {
-                this.mob.rotationYawHead = MathHelper.func_219800_b(this.mob.rotationYawHead, this.mob.renderYawOffset, (float) this.mob.getHorizontalFaceSpeed());
-            }
-        }
+    // TODO PR a event to forge that allows us to cancel look control ticking, and
+    // also pray that they don't ignore it.
+    @Inject(at = @At(value = "HEAD"), method = "tick", cancellable = true)
+    public void tick(CallbackInfo info) {
+        if (EnchantmentHelper.getEnchantmentLevel(BigBrainEnchantments.TURNING.get(), mob.getHeldItemOffhand()) == 0 && ((IBucklerUser) mob).isBucklerDashing())
+            info.cancel();
     }
 
-    @Shadow
-    private float clampedRotate(float rotationYawHead, float targetYaw, float deltaLookYaw2) {
-        return 0;
+    @Inject(at = @At(value = "HEAD"), method = "setLookPosition(DDDFF)V", cancellable = true)
+    public void setLookPosition(double x, double y, double z, float deltaYaw, float deltaPitch, CallbackInfo info) {
+        if (EnchantmentHelper.getEnchantmentLevel(BigBrainEnchantments.TURNING.get(), mob.getHeldItemOffhand()) == 0 && ((IBucklerUser) mob).isBucklerDashing())
+            info.cancel();
     }
-
-    @Shadow
-    private boolean shouldResetPitch() {
-        return false;
-    }
-
-    @Overwrite
-    public void setLookPosition(double x, double y, double z, float deltaYaw, float deltaPitch) {
-        if (EnchantmentHelper.getEnchantmentLevel(BigBrainEnchantments.TURNING.get(), mob.getHeldItemOffhand()) > 0 && ((IBucklerUser) mob).isBucklerDashing() || !((IBucklerUser) mob).isBucklerDashing()) {
-            this.posX = x;
-            this.posY = y;
-            this.posZ = z;
-            this.deltaLookYaw = deltaYaw;
-            this.deltaLookPitch = deltaPitch;
-            this.isLooking = true;
-        }
-    }
-
-    @Shadow
-    protected float getTargetPitch() {
-        double d0 = this.posX - this.mob.getPosX();
-        double d1 = this.posY - this.mob.getPosYEye();
-        double d2 = this.posZ - this.mob.getPosZ();
-        double d3 = (double) MathHelper.sqrt(d0 * d0 + d2 * d2);
-        return (float) (-(MathHelper.atan2(d1, d3) * (double) (180F / (float) Math.PI)));
-    }
-
-    @Shadow
-    protected float getTargetYaw() {
-        double d0 = this.posX - this.mob.getPosX();
-        double d1 = this.posZ - this.mob.getPosZ();
-        return (float) (MathHelper.atan2(d1, d0) * (double) (180F / (float) Math.PI)) - 90.0F;
-    }
-
 }
