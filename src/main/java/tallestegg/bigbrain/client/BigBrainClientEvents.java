ff28093101a50f08c68969ba2296570f36af5a8a
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2021-02-13 12:50:00 -0800

    1.1.3

--- src/main/java/tallestegg/bigbrain/client/BigBrainClientEvents.java
+++ src/main/java/tallestegg/bigbrain/client/BigBrainClientEvents.java
@@ -89,11 +89,11 @@ public class BigBrainClientEvents {
                     event.getMatrixStack().rotate(Vector3f.YP.rotationDegrees(180.0F - f));
                     event.getMatrixStack().scale(-1.0F, -1.0F, 1.0F);
                     double motionZ = Math.abs(entityIn.getMotion().getZ());
-                    event.getMatrixStack().translate(0.0D, -1.50D, i * motionZ * 2 / ((IBucklerUser)entityIn).getBucklerUseTimer());
+                    event.getMatrixStack().translate(0.0D, -1.50D, i * motionZ * 2 / ((IBucklerUser) entityIn).getBucklerUseTimer());
                     try {
                         preRenderCallback.invoke(event.getRenderer(), entityIn, event.getMatrixStack(), event.getPartialRenderTick());
                     } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-                        e.printStackTrace();
+                        new RuntimeException("Big Brain has failed to invoke preRenderCallback via reflection.");
                     }
                     float f8 = 0.0F;
                     float f5 = 0.0F;
