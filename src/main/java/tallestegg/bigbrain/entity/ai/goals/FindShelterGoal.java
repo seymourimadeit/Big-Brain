3bde57c0b813e4669d9ca13280b6988a264f8517
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2021-04-22 19:06:20 -0700

    removed debug things

--- src/main/java/tallestegg/bigbrain/entity/ai/goals/FindShelterGoal.java
+++ src/main/java/tallestegg/bigbrain/entity/ai/goals/FindShelterGoal.java
@@ -8,15 +8,12 @@ import net.minecraft.entity.CreatureEntity;
 import net.minecraft.entity.ai.goal.FleeSunGoal;
 import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.entity.passive.horse.AbstractHorseEntity;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.vector.Vector3d;
 import tallestegg.bigbrain.BigBrainConfig;
 
 public class FindShelterGoal extends FleeSunGoal {
-    protected double shelterX;
-    protected double shelterY;
-    protected double shelterZ;
-
     public FindShelterGoal(CreatureEntity entity) {
         super(entity, 1.35D);
     }
@@ -25,33 +22,7 @@ public class FindShelterGoal extends FleeSunGoal {
     public boolean shouldExecute() {
         boolean raining = creature.getEntityWorld().isNightTime() && !BigBrainConfig.NightAnimalBlackList.contains(creature.getEntityString()) || !BigBrainConfig.RainAnimalBlackList.contains(creature.getEntityString()) && creature.getEntityWorld().isRainingAt(creature.getPosition());
         boolean isTamed = creature instanceof TameableEntity && ((TameableEntity) creature).isTamed() || creature instanceof AbstractHorseEntity && ((AbstractHorseEntity) creature).getOwnerUniqueId() != null;
-        return !this.creature.world.isRemote() && raining && !isTamed && !creature.isBeingRidden() && creature.getAttackTarget() == null && this.creature.getEntityWorld().canSeeSky(creature.getPosition()) && this.isPossibleShelter();
-    }
-
-    @Override
-    public void startExecuting() {
-        this.creature.getNavigator().tryMoveToXYZ(this.shelterX, this.shelterY, this.shelterZ, 1.35D);
-    }
-
-    @Override
-    public boolean shouldContinueExecuting() {
-        return this.creature.hasPath();
-    }
-
-    @Override
-    public void tick() {
-    }
-
-    protected boolean isPossibleShelter() {
-        Vector3d vector3d = this.findPossibleShelter();
-        if (vector3d == null) {
-            return false;
-        } else {
-            this.shelterX = vector3d.x;
-            this.shelterY = vector3d.y;
-            this.shelterZ = vector3d.z;
-            return true;
-        }
+        return raining && !isTamed && !creature.isBeingRidden() && creature.getAttackTarget() == null && this.creature.getEntityWorld().canSeeSky(creature.getPosition()) && this.isPossibleShelter();
     }
 
     @Override
@@ -61,9 +32,9 @@ public class FindShelterGoal extends FleeSunGoal {
         BlockPos blockpos = this.creature.getPosition();
         for (int i = 0; i < 10; ++i) {
             BlockPos blockpos1 = blockpos.add(random.nextInt(20) - 10, random.nextInt(6) - 3, random.nextInt(20) - 10);
-            if (!this.creature.world.canSeeSky(blockpos1)) {
+            if (!this.creature.world.canSeeSky(blockpos1) && !this.creature.world.getFluidState(blockpos1).isTagged(FluidTags.WATER))
                 return Vector3d.copyCenteredHorizontally(blockpos1);
-            }
+
         }
         return null;
     }
