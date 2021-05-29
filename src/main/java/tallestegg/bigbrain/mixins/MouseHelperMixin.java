6c74dd4eb00b815ccc933b2514a45f8840c33138
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2021-05-28 18:29:57 -0700

    Fix bug where turning enchantment didn't remove decreased turn speed

--- src/main/java/tallestegg/bigbrain/mixins/MouseHelperMixin.java
+++ src/main/java/tallestegg/bigbrain/mixins/MouseHelperMixin.java
@@ -9,6 +9,7 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.MouseHelper;
+import tallestegg.bigbrain.BigBrainEnchantments;
 import tallestegg.bigbrain.entity.IBucklerUser;
 
 @Mixin(MouseHelper.class)
@@ -19,7 +20,6 @@ public class MouseHelperMixin {
 
     @ModifyVariable(at = @At(value = "STORE", opcode = Opcodes.DSTORE), method = "updatePlayerLook", ordinal = 2)
     public double updatePlayerLook(double original) {
-        return minecraft.player != null && ((IBucklerUser) minecraft.player).isBucklerDashing() ? 0.2F : original;
+        return minecraft.player != null && ((IBucklerUser) minecraft.player).isBucklerDashing() && BigBrainEnchantments.getBucklerEnchantsOnHands(BigBrainEnchantments.TURNING.get(), minecraft.player) == 0? 0.2F : original;
     }
-
 }
