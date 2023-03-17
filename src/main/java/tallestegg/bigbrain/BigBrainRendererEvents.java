b3ff435ef446f4f0cbe449ed9e262aa820008623
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2023-03-16 20:07:55 -0700

    added drowned glowing (like in bedrock)

---
+++ src/main/java/tallestegg/bigbrain/BigBrainRendererEvents.java
@@ -0,0 +1,18 @@
+package tallestegg.bigbrain;
+
+import net.minecraft.client.renderer.entity.DrownedRenderer;
+import net.minecraft.world.entity.EntityType;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import tallestegg.bigbrain.client.renderers.layers.DrownedGlowLayer;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class BigBrainRendererEvents {
+    @SubscribeEvent
+    public static void addRenderLayers(EntityRenderersEvent.AddLayers event) {
+        DrownedRenderer renderer = event.getRenderer(EntityType.DROWNED);
+        renderer.addLayer(new DrownedGlowLayer<>(renderer));
+    }
+}
---
+++ src/main/java/tallestegg/bigbrain/client/renderers/layers/DrownedGlowLayer.java
@@ -0,0 +1,21 @@
+package tallestegg.bigbrain.client.renderers.layers;
+
+import net.minecraft.client.model.DrownedModel;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.client.renderer.entity.layers.EyesLayer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.monster.Drowned;
+import tallestegg.bigbrain.BigBrain;
+
+public class DrownedGlowLayer<T extends Drowned, M extends DrownedModel<T>> extends EyesLayer<T, M> {
+    private static final RenderType SPIDER_EYES = RenderType.eyes(new ResourceLocation(BigBrain.MODID, "textures/entity/zombie/drowned_glow_layer.png"));
+
+    public DrownedGlowLayer(RenderLayerParent<T, M> p_117507_) {
+        super(p_117507_);
+    }
+
+    public RenderType renderType() {
+        return SPIDER_EYES;
+    }
+}
