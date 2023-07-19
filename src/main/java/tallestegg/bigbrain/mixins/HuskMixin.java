6080401365213d9b67d4cc958e77079de93dc5a6
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2023-07-19 13:56:43 -0700

    fix husk burrow

--- src/main/java/tallestegg/bigbrain/mixins/HuskMixin.java
+++ src/main/java/tallestegg/bigbrain/mixins/HuskMixin.java
@@ -19,15 +19,4 @@ public abstract class HuskMixin extends Zombie {
     public boolean canRiderInteract() {
         return true;
     }
-
-    @Override
-    public void positionRider(Entity pPassenger, Entity.MoveFunction pCallback) {
-        super.positionRider(pPassenger);
-        float f = Mth.sin(this.yBodyRot * ((float)Math.PI / 180F));
-        float f1 = Mth.cos(this.yBodyRot * ((float)Math.PI / 180F));
-        pPassenger.setPos(this.getX() + (double)(0.1F * f), this.getY(0.5D) + pPassenger.getMyRidingOffset() + 0.0D, this.getZ() - (double)(0.1F * f1));
-        if (pPassenger instanceof LivingEntity) {
-            ((LivingEntity)pPassenger).yBodyRot = this.yBodyRot;
-        }
-    }
 }
