0e1bda1fd580a78d5ebcd534566cd935290de474
seymourimadeit <anemailaccount@mail.com>
2024-06-12 19:28:04 -0700

    fix #50

--- src/main/java/tallestegg/bigbrain/client/BigBrainSounds.java
+++ src/main/java/tallestegg/bigbrain/client/BigBrainSounds.java
@@ -10,8 +10,4 @@ import tallestegg.bigbrain.BigBrain;
 
 public class BigBrainSounds {
     public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(Registries.SOUND_EVENT, BigBrain.MODID);
-    public static final Holder<SoundEvent> PIGLIN_BRUTE_CHARGE = SOUNDS.register("entity.piglin_brute.charge", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(BigBrain.MODID, "entity.piglin_brute.charge")));
-    public static final Holder<SoundEvent> SHIELD_BASH = SOUNDS.register("item.buckler.bash", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(BigBrain.MODID, "item.buckler.bash")));
-    public static final Holder<SoundEvent> CRITICAL_ACTIVATE = SOUNDS.register("entity.criticalcharge.activate", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(BigBrain.MODID, "entity.criticalcharge.activate")));
-    public static final Holder<SoundEvent> CRITICAL_DEACTIVATE = SOUNDS.register("entity.criticalcharge.deactivate", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(BigBrain.MODID, "entity.criticalcharge.deactivate")));
 }
