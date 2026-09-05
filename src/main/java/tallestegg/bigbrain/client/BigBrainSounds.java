6d0a789f645c13bd7e1961dbade08c3d9e976531
seymourimadeit <saulgoodman53434344@gmail.com>
2026-09-04 18:50:34 -0700

    [CHANGE] future proofed Drowned and Armadillo render layer addition code to prevent crashing

--- src/main/java/tallestegg/bigbrain/client/BigBrainSounds.java
+++ src/main/java/tallestegg/bigbrain/client/BigBrainSounds.java
@@ -1,10 +1,8 @@
 package tallestegg.bigbrain.client;
 
-import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
-import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import tallestegg.bigbrain.BigBrain;
